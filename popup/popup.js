31e01e85797715cfb8383f1610c247d2a37804cb
Yogesh Limbani <yrlimbani03@gmail.com>
2023-09-10 23:14:36 +0530

    Update

---
+++ popup/popup.js
@@ -0,0 +1,9 @@
+let clicked= false;
+document.getElementById('btn').addEventListener('click', function() {
+    if(!clicked){
+        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { act: "disp" });
+        });
+    }
+    clicked = true;
+});
---
+++ scripts/contentScript.js
@@ -0,0 +1,37 @@
+chrome.runtime.onMessage.addListener((obj, sender, sendResponse) => {
+    if (obj.act == "disp"){
+        const body = document.querySelector(".table.mb-0 tbody");
+        const rows = body.querySelectorAll('tr');
+        for (var i = 0; i < rows.length - 1; i++) {
+            var lastChild = rows[i].querySelector('td:last-child');
+            var maxhrs = rows[i].querySelector('td:nth-child(3)').textContent;
+            var prshrs = rows[i].querySelector('td:nth-child(4)').textContent;
+            var lastChildValue = lastChild.textContent;
+            if(Number(lastChildValue) < 75){
+                lastChild.style.color = 'red';
+                let toadd = 0;
+                let lcv = lastChildValue;
+                while(lcv < 75){
+                    prshrs++;
+                    maxhrs++;
+                    lcv = (Number(prshrs)*100)/Number(maxhrs);
+
+                    toadd = toadd + 1;
+                }
+                lastChild.textContent = lastChildValue +" ("+ toadd + ")"; 
+            }
+            else{
+                lastChild.style.color = 'green';
+                let lcv = lastChildValue;
+                let toadd = -1;
+                while(lcv >= 75){
+                    maxhrs++;
+                    lcv = (Number(prshrs)*100)/Number(maxhrs);
+                    toadd = toadd + 1;
+                }
+                lastChild.textContent = lastChildValue +" ("+ toadd + ")"; 
+            }
+        }
+    }
+
+}); 
